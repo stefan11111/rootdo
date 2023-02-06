75ae67576328db49f082d4a0848f4b006c9d5f14
stefan11111 <stefanalexe48@gmail.com>
2023-02-07 01:21:00 +0200

    make the code more pretty

--- rdo.c
+++ rdo.c
@@ -1,20 +1,20 @@
 #include "rdo.h"
 
-int run_program(char **program){
-    if (geteuid() != 0){
+int run_program(char **program) {
+    if (geteuid() != 0) {
         printf("The rdo binary needs to be installed as SUID. \n");
         return 1;
     }
-    if (setgid(0) < 0){
+    if (setgid(0) < 0) {
         printf("Could not setuid.\n");
         return -1;
     }
-    if (setuid(0) < 0){
+    if (setuid(0) < 0) {
         printf("Could not setgid.\n");
         return -1;
     }
     putenv("HOME=/root");
-    if (execvp(*program, program) == -1){
+    if (execvp(*program, program) == -1) {
         printf("%s: command not found\n", *program);
         return -1;
     }
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
 
     int ruid = getuid();
 
-    if(!ruid && ALLOW_ROOT){
+    if (!ruid && ALLOW_ROOT) {
 	putenv("HOME=/root");
 	execvp(argv[1], argv + 1);
 	return 0;
@@ -37,12 +37,12 @@ int main(int argc, char** argv) {
 
     struct passwd *user = getpwuid(ruid);
 
-    if(strcmp(user->pw_name, ALLOWED_USER)){
+    if (strcmp(user->pw_name, ALLOWED_USER)) {
 	printf("You are not the allowed user.\n");
 	return 1;
     }
 
-    if(!REQUIRE_PASSWORD){
+    if (!REQUIRE_PASSWORD) {
 	return run_program(argv + 1);
     }
 
@@ -52,9 +52,10 @@ int main(int argc, char** argv) {
     term.c_lflag &= ~ECHO;
     tcsetattr(1, 0, &term);
     term.c_lflag |= ECHO;
-    if (write(1, "Enter the password: ", 20) < 0)
+    if (write(1, "Enter the password: ", 20) < 0) {
 	tcsetattr(1, 0, &term);
-    if(scanf("%200s", pass) != 1){
+    }
+    if (scanf("%200s", pass) != 1) {
 	printf("Error reading password.\n");
 	tcsetattr(1, 0, &term);
 	return 0;
@@ -64,19 +65,19 @@ int main(int argc, char** argv) {
 
     struct spwd* shadow = getspnam(user->pw_name);
 
-    if (!shadow || !shadow->sp_pwdp){
+    if (!shadow || !shadow->sp_pwdp) {
         printf("Could not get shadow entry.\n");
 	return 1;
     }
 
     char *hashed = NULL;
     hashed = crypt(pass, shadow->sp_pwdp);
-    if(!hashed){
+    if (!hashed) {
 	printf("Could not hash password, does your user have a password?");
 	return 1;
     }
 
-    if(strcmp(hashed, shadow->sp_pwdp)){
+    if (strcmp(hashed, shadow->sp_pwdp)) {
 	printf("Wrong password.\n");
 	return 1;
     }
--- rdo.noconfig.c
+++ rdo.noconfig.c
@@ -8,22 +8,22 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    if (geteuid() != 0){
+    if (geteuid() != 0) {
         printf("The rdo binary needs to be installed as SUID. \n");
 	return 1;
     }
 
-    if (setgid(0) < 0){
+    if (setgid(0) < 0) {
         printf("Could not setuid\n");
 	return 1;
     }
-    if (setuid(0) < 0){
+    if (setuid(0) < 0) {
         printf("Could not setgid\n");
 	return 1;
     }
 
     putenv("HOME=/root");
-    if(execvp(argv[1], argv + 1) == -1){
+    if (execvp(argv[1], argv + 1) == -1) {
         printf("%s: command not found\n", argv[1]);
         return 1;
     }
